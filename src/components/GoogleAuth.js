f68425a69cf8ec3222b43e2bffc185b381e0a75a
Dennie Nguyen <streamvillecanada@gmail.com>
2021-01-24 00:46:56 -0500

    add clientId message

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -6,7 +6,7 @@ class GoogleAuth extends React.Component{
   componentDidMount(){
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
-        clientId: '',
+        clientId: 'ADD_KEY_HERE',
         scope: 'email'
       })
       .then(() => {
